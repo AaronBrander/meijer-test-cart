f65e622326e48eff46aea1d6a51ddf53d606f8d0
Aaron Brander <aaron@utmsoft.com>
2020-10-23 12:12:28 -0400

    displaying on cart screen and removing from cart

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import GlobalContext from "./GlobalContext";
 import API from "./API";
 
-
 class GlobalState extends Component {
   state = {
     profile: {},
@@ -10,17 +9,15 @@ class GlobalState extends Component {
     products: [],
     cart: [],
     showAlert: false,
-    alertMessage: ''
+    alertMessage: "",
   };
 
-  setShowAlert = (isVisible) => 
-  {
-    this.setState({showAlert: isVisible});
+  setShowAlert = (isVisible) => {
+    this.setState({ showAlert: isVisible });
   };
 
-  setAlertMessage = (msg) =>
-  {
-    this.setState({alertMessage: msg});
+  setAlertMessage = (msg) => {
+    this.setState({ alertMessage: msg });
   };
 
   getProducts = () => {
@@ -34,49 +31,59 @@ class GlobalState extends Component {
   };
 
   updateCart = (product, quantity) => {
-    if(quantity === undefined)
-    {
+    if (quantity === undefined) {
       quantity = 1;
     }
 
-    console.log('updateCart');
-    const cartItems = this.state.cart.slice(); //immutability is important!
+    console.log("updateCart");
+    if (quantity === 0) {
+      //remove from cart
+      const remainingItems = this.state.cart.filter(x => x.code !== product.code);
+      this.setShowAlert(true);
+      this.setAlertMessage("Removed " + product.name + " from your cart");
 
-    //is the item in the cart already?
-    let findCartItem = cartItems.find(x => x.code === product.code);
-    console.log(findCartItem);
-    if(findCartItem)
-    {
-      findCartItem.quantity += quantity;
-      findCartItem.lineTotal = parseFloat(product.price.substring(1)) * quantity;
-    }else
-    {
-      cartItems.push({
-        name: product.name,
-        price: product.price,
-        image: product.image,
-        code: product.code,
-        quantity: quantity,
-        lineTotal: parseFloat(product.price.substring(1)) * quantity,
-      });
+      this.setState({ cart: remainingItems });
+    } 
+    else {
+      
+      const cartItems = this.state.cart.slice(); 
 
+      //is the item in the cart already?
+      let findCartItem = cartItems.find((x) => x.code === product.code);
+      console.log(findCartItem);
+      if (findCartItem) {
+        findCartItem.quantity += quantity;
+        findCartItem.lineTotal =
+          parseFloat(product.price.substring(1)) * quantity;
+      } else {
+        cartItems.push({
+          name: product.name,
+          price: product.price,
+          image: product.image,
+          code: product.code,
+          quantity: quantity,
+          lineTotal: parseFloat(product.price.substring(1)) * quantity,
+        });
+      }
+
+      this.setState({ cart: cartItems });
     }
-
-  this.setState({ cart: cartItems });
   };
-  
+
   signOut = () => {
-    this.setState({ profile: {}, isLoggedIn: false});
+    this.setState({ profile: {}, isLoggedIn: false });
     this.setShowAlert(true);
-     this.setAlertMessage("You've been logged out!");
-  }
-  
-  signIn = () =>
-  {
-     this.setState({ profile: { firstName: "Test", lastName: "User"}, isLoggedIn: true});
-     this.setShowAlert(true);
-     this.setAlertMessage("Welcome, Test!");
-  }
+    this.setAlertMessage("You've been logged out!");
+  };
+
+  signIn = () => {
+    this.setState({
+      profile: { firstName: "Test", lastName: "User" },
+      isLoggedIn: true,
+    });
+    this.setShowAlert(true);
+    this.setAlertMessage("Welcome, Test!");
+  };
 
   render() {
     return (
@@ -94,7 +101,7 @@ class GlobalState extends Component {
           updateProfile: this.updateProfile,
           updateCart: this.updateCart,
           signOut: this.signOut,
-          signIn: this.signIn
+          signIn: this.signIn,
         }}
       >
         {this.props.children}
--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useContext, Fragment } from "react";
 import {
-   Container,
+    Row,
+    Col,
+    Table,
+    Jumbotron,
+    Button
 } from "reactstrap";
-import ProductCart from "../components/ProductCart";
+import { Link } from "react-router-dom";
+import GlobalContext from "../GlobalContext";
 
 const Cart = (props) => {
-
+    const { cart, updateCart } = useContext(GlobalContext);
+    
     return (
-        <Container>
-            <ProductCart></ProductCart>
-            <ProductCart></ProductCart>
-            <ProductCart></ProductCart>
-           
-        </Container>
+        <Fragment>
+            {(cart && cart.length > 0) &&
+                <Row className="pt-4">
+                    <Col>
+                        <h2>Your Cart</h2>
+                        <Table responsive>
+                            <thead>
+                                <th></th>
+                                <th>Product</th>
+                                <th>Quantity</th>
+                                <th>Total</th>
+                                <th>Delete</th>
+                            </thead>
+                            <tbody>
+                                {cart.map((item, index) => (
+                                    <tr key={item.code}>
+                                        <td>
+                                            <Button onClick={() => updateCart(item, 0)}>Delete</Button>
+                                    </td>
+                                        <td>
+                                            <img width="100px" src={item.image} alt={item.name} />
+                                        </td>
+                                        <td>{item.name}</td>
+                                        <td>{item.quantity}</td>
+                                        <td>${item.lineTotal}</td>
+
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    </Col>
+
+                </Row>
+            }
+            {(!cart || cart.length === 0) &&
+                <Jumbotron><h2>Your cart is empty.</h2>
+                    <p>Try adding some <Link to="/">products.</Link> </p>
+                </Jumbotron>
+            }
+        </Fragment>
     );
 
 }
